6ca2126b87fd77a35ad030e196d1ee29a600cd91
khanhdayr002 <154343426+khanhdayr002@users.noreply.github.com>
2024-05-08 23:18:05 +0700

    Update facebook.js

--- public/info/facebook.js
+++ public/info/facebook.js
@@ -5,7 +5,7 @@ var uid = req.query.uid
 var tokenU = req.query.token
 if(!uid) return ress.json({ error: 'thiếu dữ liệu uid'})
 try {
-const token = 'EAAGNO4a7r2wBAJfj8vzYOuD7fOvDj7iz6rGD00yHXHEgjsbX0pIaZC1QCKhTHF3pibvuVr8h7Ef3Kn7u27ZCQrO0KSA6Qbi2iZClsgknrujTaZAeZBeU0wZBjetLpCqJnI9HwIVO2ckqIkPjG4PXUmCDS9dXSDOhtgxOfFCBbJwKqzGD6r1y0M'
+const token = 'EAAAAUaZA8jlABO4v044jFrYsRwIXZBNHh1aJFPZAauyhQyLwRuu3SlCbiCAPmOgoRrcbqmzaszZBqfQRIeOU6sz0Iz1XkrHJr9OK1DuwhM9Kak4IWI9AZAZBCN226ZA3Dh0fg0zk9zyiZC50WsgpCQU5OFlWlggZBrcEzQAL6qJSrP1josM5LVZBZCErroBHmcJRZBoGgAZDZD'
 const options = {
   method: 'POST',
   url: 'https://get-info-user-facebook.p.rapidapi.com/api-no-key/info',
